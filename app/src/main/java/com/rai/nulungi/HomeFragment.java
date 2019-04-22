568c3e58c9a72993e55458f318f787386a3a63b5
radityarin <radityarin@gmail.com>
2019-04-22 22:06:36 +0700

    Initial commit

---
+++ app/src/main/java/com/rai/nulungi/HomeFragment.java
@@ -0,0 +1,48 @@
+package com.rai.nulungi;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+public class HomeFragment extends Fragment {
+
+    ViewFlipper viewFlipper;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        final View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+//        int images [] = {R.drawable.testslidephoto,R.drawable.testslidephoto,R.drawable.testslidephoto};
+//
+//        viewFlipper = view.findViewById(R.id.slideshowphoto);
+//
+//        for (int image: images){
+//            flipperImages(image);
+//        }
+
+        return view;
+    }
+
+    public void flipperImages(int image){
+        ImageView imageView = new ImageView(getContext());
+        imageView.setBackgroundResource(image);
+
+        viewFlipper.addView(imageView);
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.setAutoStart(true);
+
+        viewFlipper.setInAnimation(getContext(),android.R.anim.slide_in_left);
+        viewFlipper.setOutAnimation(getContext(),android.R.anim.slide_out_right);
+    }
+
+}
---
+++ app/src/main/java/com/rai/nulungi/MainActivity.java
@@ -0,0 +1,52 @@
+package com.rai.nulungi;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+            switch (item.getItemId()) {
+                case R.id.homebutton:
+                    HomeFragment homeFragment = new HomeFragment();
+                    fragmentTransaction.replace(R.id.main_frame, homeFragment, "Home Fragment");
+                    fragmentTransaction.commit();
+                    setTitle("Home");
+                    return true;
+                case R.id.history:
+                    HistoryFragment historyFragment = new HistoryFragment();
+                    fragmentTransaction.replace(R.id.main_frame, historyFragment,"Histori Fragment");
+                    fragmentTransaction.commit();
+                    setTitle("History");
+                    return true;
+//                case R.id.inboxbutton:
+//                    InboxFragment inboxFragment = new InboxFragment();
+//                    fragmentTransaction.replace(R.id.main_frame, inboxFragment, "Inbox Fragment");
+//                    fragmentTransaction.commit();
+//                    setTitle("Inbox");
+//                    return true;
+                case R.id.profilebutton:
+                    ProfileFragment profileFragment = new ProfileFragment();
+                    fragmentTransaction.replace(R.id.main_frame, profileFragment, "Profile Fragment");
+                    fragmentTransaction.commit();
+                    setTitle("Profile");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
